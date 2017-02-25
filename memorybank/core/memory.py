702add74aeccf0c038a15de4b11c8d52759d5e71
Mikael Hallendal <hallski@hallski.org>
2017-02-25 11:08:59 +0100

    Add start idea for API to connect memories

--- memorybank/core/memory.py
+++ memorybank/core/memory.py
@@ -1,5 +1,15 @@
 # Copyright (C) 2017 Mikael Hallendal <hallski@hallski.org>
 
 class Memory(object):
-    def __init__(self, title):
+    def __init__(self, title, note=''):
         self.title = title
+        self.note = note
+
+    def add_connection(self, other, connection_type):
+        pass
+
+    def remove_connection(self, other, connection_type=''):
+        pass
+
+    def get_connections(self, connection_type=''):
+        pass
