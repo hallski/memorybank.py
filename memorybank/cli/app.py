743ea49a926ce277ff605282f46ecb9a0ad14ee4
Mikael Hallendal <hallski@hallski.org>
2017-02-26 01:01:00 +0100

    More testing of layout and added better mock data

--- memorybank/cli/app.py
+++ memorybank/cli/app.py
@@ -2,10 +2,10 @@
 
 import urwid
 
-parents = ['Parent 1', 'Parent 2', 'Parent 3']
-children = ['Child 1', 'Child 2', 'Child 3', 'Child 4', 'Child 5']
-siblings = ['Sibling 1', 'Sibling 2', 'Sibling 3', 'Sibling 4', 'Sibling 5']
-related = ['Related 1', 'Related 2', 'Related 3']
+parents = ['Blizzard Entertainment', 'Roleplaying Games', 'MMORPG', 'Fantasy']
+children = ['Azeroth', 'Illidan', 'Wrath of the Litch King', 'Draenor']
+siblings = ['Diablo', 'Starcraft', 'Warcraft', 'Guild Wars', 'Rift']
+related = ['Warcraft', 'MMO Champion', 'Dark Legacy Comics']
 
 palette = [
     ('debug1', 'yellow', 'dark red'),
@@ -16,7 +16,7 @@ palette = [
     ('background', 'light green', 'black')]
 
 
-active_memory = urwid.Text('Focused memory', align='center')
+active_memory = urwid.Text('World of Warcraft', align='center')
 
 class MBButton(urwid.Button):
     def __init__(self, caption, callback):
@@ -51,24 +51,22 @@ def wrap_with_palette(palette_name):
     def curried_decorator(func):
         def wrapper(*args, **kwargs):
             widget = func(*args, **kwargs)
-            print('pn:', widget)
             mapped = urwid.AttrMap(widget, palette_name)
-            print('pp:', mapped)
             return mapped
         return wrapper
     return curried_decorator
 
-@wrap_with_palette('background')
-def get_active_memory():
-    return urwid.Filler(active_memory, 'middle')
-
-
 @wrap_with_palette('debug4')
+def get_active_memory():
+    return urwid.Filler(active_memory, 'top')
+
+
+#@wrap_with_palette('debug4')
 def get_parents_widget():
     return get_list_widget(parents)
 
 
-@wrap_with_palette('debug2')
+#@wrap_with_palette('debug2')
 def get_chilren_widget():
     return get_list_widget(children)
 
@@ -78,22 +76,29 @@ def get_list_widget(items):
     widget = urwid.ListBox(walker)
     return widget
 
-@wrap_with_palette('debug3')
+#@wrap_with_palette('debug3')
 def get_related_widget():
     return get_list_widget(related)
 
 
-@wrap_with_palette('debug5')
+#@wrap_with_palette('debug5')
 def get_siblings_widget():
     return get_list_widget(siblings)
 
 
 def main():
-    col = urwid.Columns([('weight', 1, get_related_widget()),
-                         ('weight', 2, get_active_memory()),
-                         ('weight', 1, get_siblings_widget())])
+    under_cols = urwid.Columns([('weight', 1, get_related_widget()),
+                                ('weight', 2, get_chilren_widget()),
+                                ('weight', 1, get_siblings_widget())],
+                               dividechars=5)
+    main_pile = urwid.Pile([get_parents_widget(),
+                            urwid.LineBox(get_active_memory()),
+                            urwid.Padding(under_cols,
+                                          left=2, right=2)])
+#    col = urwid.Columns([('weight', 1, get_related_widget()),
+#                         ('weight', 2, main_pile),
+#                         ('weight', 1, get_siblings_widget())])
 
-    main_pile = urwid.Pile([get_parents_widget(), col, get_chilren_widget()])
     background = urwid.AttrMap(main_pile, 'background')
     loop = urwid.MainLoop(background, palette, unhandled_input=exit_on_q)
     loop.run()
