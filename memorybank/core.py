7709645f374ce15bc17c649fa8d9ef70e4de81d8
Mikael Hallendal <hallski@hallski.org>
2017-03-03 21:49:32 +0100

    Started working a bit on the MemoryBank class
    This is the in the core module and will serve as the main API to
    manipulate the data base. The API will need to be changed to be
    asynchronous later but this serves for now.

--- memorybank/core.py
+++ memorybank/core.py
@@ -1,6 +1,42 @@
 # Copyright (C) 2017 Mikael Hallendal <hallski@hallski.org>
 
 
+class MemoryBank(object):
+    def __init__(self):
+        self._connections = []
+
+    def create_memory(self, title, note=''):
+        return Memory(title, note=note)
+
+    def connect(self, memory_a, memory_b, type='child'):
+        self._connections.append(Connection(memory_a, memory_b, type))
+
+    def get_connections(self, memory):
+        return [(c.memory_b, c.connection_type) for c in self._connections if c.memory_a == memory]
+
+    def find_memory(self, title):
+        return Memory('Memory to find')
+
+
+class Connection(object):
+    def __init__(self, memory_a, memory_b, connection_type):
+        self._memory_a = memory_a
+        self._memory_b = memory_b
+        self._connection_type = connection_type
+
+    @property
+    def memory_a(self):
+        return self._memory_a
+
+    @property
+    def memory_b(self):
+        return self._memory_b
+
+    @property
+    def connection_type(self):
+        return self._connection_type
+
+
 class Memory(object):
     def __init__(self, title, note=''):
         self._title = title
@@ -22,3 +58,14 @@ class Memory(object):
 
     def get_connections(self, connection_type=''):
         pass
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.title == other.title and \
+                   self.note == other.note
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
--- tests/memorybank_tests.py
+++ tests/memorybank_tests.py
@@ -1,8 +1,37 @@
 from nose.tools import *
-from memorybank.core import Memory
+from memorybank.core import *
+import unittest
 
 
-def test_memory():
-    memory = Memory('A memory')
-    assert_equal(memory.title, 'A memory')
+class MemoryBankTest(unittest.TestCase):
+    def setUp(self):
+        self.mbank = MemoryBank()
 
+    def test_create_memory(self):
+        memory = self.mbank.create_memory('Creating a new memory')
+        assert_equal(memory.title, 'Creating a new memory')
+
+    def test_find_memory(self):
+        memory = self.mbank.create_memory('Memory to find')
+        found_memory = self.mbank.find_memory('Memory to find')
+        assert_equal(memory, found_memory)
+
+    def test_connect_memories(self):
+        memory_a = self.mbank.create_memory('Something to remember')
+        memory_b = self.mbank.create_memory('Another thing to remember')
+        self.mbank.connect(memory_a, memory_b, type='child')
+
+        connections = self.mbank.get_connections(memory_a)
+        assert_equal(connections[0], (memory_b, 'child'))
+
+    def test_connect_memories_twice(self):
+        memory_a = self.mbank.create_memory('First memory')
+        memory_b = self.mbank.create_memory('Second memory')
+
+        self.mbank.connect(memory_a, memory_b, type='child')
+        self.mbank.connect(memory_a, memory_b, type='related')
+
+        connections = self.mbank.get_connections(memory_a)
+        assert_equal(len(connections), 2)
+        assert((memory_b, 'child') in connections)
+        assert((memory_b, 'related') in connections)
